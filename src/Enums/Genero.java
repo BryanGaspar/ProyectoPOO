0d73a44bf12f466324042377d692bee3c3a55a67
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2022-01-27 18:28:11 -0500

    COMMENTED ENUMS

--- src/Enums/Genero.java
+++ src/Enums/Genero.java
@@ -1,13 +1,35 @@
 package Enums;
-
+/**
+ * @author Brian Bastidas
+ * Se crea el enum Genero
+ */
 public enum Genero {
-	MALE("Masculino"), FEMALE("Femenino"), OTHER("Otro");
+	/**
+	 * Decalarar enum MALE
+	 */
+	MALE("Masculino"),
+	/**
+	 * Decalarar enum MALE
+	 */
+	FEMALE("Femenino"),
+	/**
+	 * Decalarar enum OTHER
+	 */
+	OTHER("Otro");
+	/**
+	 * Se decalata variable String
+	 */
 	private String imp_genero;
-	
+	/**
+	 * 
+	 * @param Recibe la variable s
+	 */
 	Genero(String s) {
 		imp_genero = s;
 	}
-	
+	/**
+	 * Se delcara metodo ToString
+	 */
 	public String toString() {
 		return imp_genero;
 	}
--- src/Enums/Niveles.java
+++ src/Enums/Niveles.java
@@ -1,16 +1,56 @@
 package Enums;
-
+/**
+ * @author Brian Bastidas
+ * Se crea el enum Niveles
+ */
 public enum Niveles {
-	Primero("1er. Nivel"), Segundo("2do. Nivel"), Tercero("3er. Nivel"), Cuarto("4to. Nivel"), Quinto("5to. Nivel"),
-	Sexto("6to. Nivel"), Septimo("7to. Nivel"), Octavo("8to. Nivel");
+	/**
+	 * Se delcara el enum Primero
+	 */
+	Primero("1er. Nivel"),
+	/**
+	 * Se delcara el enum Segundo
+	 */
+	Segundo("2do. Nivel"),
+	/**
+	 * Se delcara el enum Tercero
+	 */
+	Tercero("3er. Nivel"),
+	/**
+	 * Se delcara el enum Cuarto
+	 */
+	Cuarto("4to. Nivel"),
+	/**
+	 * Se delcara el enum Quinto
+	 */
+	Quinto("5to. Nivel"),
+	/**
+	 * Se delcara el enum Sexto
+	 */
+	Sexto("6to. Nivel"),
+	/**
+	 * Se delcara el enum Septimo
+	 */
+	Septimo("7to. Nivel"),
+	/**
+	 * Se delcara el enum Octavo
+	 */
+	Octavo("8to. Nivel");
+	/**
+	 * Se decalata variable String
+	 */
 	private String imp_nivel;
-	
+	/**
+	 * 
+	 * @param Recibe la variable s
+	 */
 	Niveles(String s) {
 		imp_nivel = s;
 	}
-	
+	/**
+	 * Se delcara metodo ToString
+	 */
 	public String toString() {
 		return imp_nivel;
-	}
-	
+	}	
 }
