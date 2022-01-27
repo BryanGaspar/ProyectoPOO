52c67c1d5fa41f95614586bf3b7c5ed881e85f25
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2022-01-27 18:15:46 -0500

    CLASES COMMENTED

--- src/Clases/Estudiante.java
+++ src/Clases/Estudiante.java
@@ -1,6 +1,12 @@
 package Clases;
-
+/**
+ * @author Brian Bastidas
+ * Se crea la class Estudiante
+ */
 public class Estudiante {
+	/**
+	 * Se declaran variables
+	 */
 	private int cedula;
 	private String nombres;
 	private String apellidos;
@@ -9,53 +15,116 @@ public class Estudiante {
 	private int telefono;
 	private String genero;
 	private String fechaNac;
+	/**
+	 * Metodos getters
+	 * @param admin
+	 */
 	public int getCedula() {
 		return cedula;
 	}
+	/**
+	 * Metodos setters 
+	 * @param admin
+	 */
 	public void setCedula(int cedula) {
 		this.cedula = cedula;
 	}
+	/**
+	 * Metodos getters
+	 * @param admin
+	 */
 	public String getNombres() {
 		return nombres;
 	}
+	/**
+	 * Metodos setters 
+	 * @param admin
+	 */
 	public void setNombres(String nombres) {
 		this.nombres = nombres;
 	}
+	/**
+	 * Metodos getters
+	 * @param admin
+	 */
 	public String getApellidos() {
 		return apellidos;
 	}
+	/**
+	 * Metodos setters 
+	 * @param admin
+	 */
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
+	/**
+	 * Metodos getters
+	 * @param admin
+	 */
 	public String getDireccion() {
 		return direccion;
 	}
+	/**
+	 * Metodos setters 
+	 * @param admin
+	 */
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+	/**
+	 * Metodos getters
+	 * @param admin
+	 */
 	public String getEmail() {
 		return email;
 	}
+	/**
+	 * Metodos setters 
+	 * @param admin
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	/**
+	 * Metodos getters
+	 * @param admin
+	 */
 	public int getTelefono() {
 		return telefono;
 	}
+	/**
+	 * Metodos setters 
+	 * @param admin
+	 */
 	public void setTelefono(int telefono) {
 		this.telefono = telefono;
 	}
+	/**
+	 * Metodos getters
+	 * @param admin
+	 */
 	public String getGenero() {
 		return genero;
 	}
+	/**
+	 * Metodos setters 
+	 * @param admin
+	 */
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
+	/**
+	 * Metodos getters
+	 * @param admin
+	 */
 	public String getFechaNac() {
 		return fechaNac;
 	}
+	/**
+	 * Metodos setters 
+	 * @param admin
+	 */
 	public void setFechaNac(String fechaNac) {
 		this.fechaNac = fechaNac;
 	}
-
 }
--- src/Clases/administradores.java
+++ src/Clases/administradores.java
@@ -1,23 +1,40 @@
 package Clases;
-
+/**
+ * @author Brian Bastidas
+ * Se crea la class administradores
+ */
 public class administradores {
+	/**
+	 * Se declaran variables string
+	 */
 		private String admin;
 		private String pass;
-		
+		/**
+		 * Metodos setters 
+		 * @param admin
+		 */
 		public void setAdmin(String admin) {
 			this.admin = admin;
 		}
+		/**
+		 * Metodos setters 
+		 * @param admin
+		 */
 		public void setPass(String pass) {
 			this.pass = pass;
 		}
+		/**
+		 * Metodos getters
+		 * @param admin
+		 */
 		public String getAdmin() {
 			return admin;
 		}
-		
+		/**
+		 * Metodos getters
+		 * @param admin
+		 */
 		public String getPass() {
 			return pass;
-		}
-	
-		
-		
+		}		
 }
